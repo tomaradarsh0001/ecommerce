a63e847afe26d6d9582f8bb0926aa11e7e5a134a
Adarsh Tomar <tomaradarsh0001@gmail.com>
2025-07-24 17:51:55 +0530

    rbac added

--- eommerce/database/seeders/DatabaseSeeder.php
+++ eommerce/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,9 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $admin = Role::create(['name' => 'admin']);
+    $user = Role::create(['name' => 'user']);
+
+    // Create permissions
+    Permission::create(['name' => 'view dashboard']);
+    Permission::create(['name' => 'edit articles']);
+
+    // Assign permissions to roles
+    $admin->givePermissionTo(['view dashboard', 'edit articles']);
+    $user->givePermissionTo(['view dashboard']);
     }
 }
--- eommerce/routes/web.php
+++ eommerce/routes/web.php
@@ -7,9 +7,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return 'Welcome Admin!';
+    });
+});
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard' , function () {
+    return view('dashboard');    })->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
